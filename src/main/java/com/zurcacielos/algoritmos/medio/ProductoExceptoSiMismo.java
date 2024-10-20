5e60bda363cf43903c78410ea50c6b32f754b907
zurcacielos <fabianse@gmail.com>
2024-10-20 04:07:13 -0300

    documentación

--- src/main/java/com/zurcacielos/algoritmos/medio/ProductoExceptoSiMismo.java
+++ src/main/java/com/zurcacielos/algoritmos/medio/ProductoExceptoSiMismo.java
@@ -8,16 +8,20 @@ public class ProductoExceptoSiMismo {
         int n = nums.length;
         int[] res = new int[n]; // arreglo de salida
 
+        // 1. calcula los prefijos izquierdos, es decir, el producto de los elementos a la izquierda
+        // de cada elemento
         res[0] = 1; // el primer prefijo es 1, pues no hay nada a la izquierda del 1.er elemento
         for (int i = 1; i < n; i++) {
             res[i] = res[i - 1] * nums[i - 1]; // producto prefijo izquierdo por elemento actual
         }
 
+        // 2. calcula los posfijos derechos, es decir, el producto de los elementos a la derecha de
+        // cada elemento, y multiplica lo que ya está almacenado en res, para obtener el resultado
+        // final "en el lugar"
         int posfijo = 1; // primer posfijo en 1, pues no hay nada a la derecha del último elemento
         // recorre de derecha a izquierda
         for (int i = n - 1; i >= 0; i--) {
-            // la respuesta final es el producto del prefijo izquierdo "en el lugar" por el posfijo derecho
-            res[i] *= posfijo; // producto del elemento actual por postfijo de la derecha
+            res[i] *= posfijo; // producto del elemento actual, o sea prefijo izquierdo, por postfijo de la derecha
             posfijo *= nums[i]; // calcula posfijo de la derecha
         }
         return res;
