c9a9d312c00c7a75c25f9e5bd1e07393bd476134
zurcacielos <fabianse@gmail.com>
2024-10-27 03:40:59 -0300

    cambio en monedas - 2

--- src/main/java/com/zurcacielos/algoritmos/programaciondinamica/CambioEnMonedas.java
+++ src/main/java/com/zurcacielos/algoritmos/programaciondinamica/CambioEnMonedas.java
@@ -1,5 +1,7 @@
 package com.zurcacielos.algoritmos.programaciondinamica;
 
+import java.util.Arrays;
+
 // leetcode 322 cambio en monedas
 // En programación dinámica, la variable dp (abreviatura de "dynamic programming")
 // se utiliza como una tabla o arreglo para almacenar los resultados de subproblemas
@@ -10,30 +12,23 @@ public class CambioEnMonedas {
     // Utiliza un enfoque de programación dinámica de abajo hacia arriba.
     // En este enfoque, la función comienza resolviendo los subproblemas más pequeños
     // y utiliza sus soluciones para construir soluciones a subproblemas más grandes
-    public static int cambio(int[] monedas, int cantidad) {
-        int[] dp = new int[cantidad + 1];
-        // i es la cantidad para la cual se está calculando el número mínimo de monedas.
-        for (int cantActual = 1; cantActual <= cantidad; cantActual++) {
-            dp[cantActual] = Integer.MAX_VALUE;
-            // recorre todas las denominaciones de monedas para
-            // determinar cuál es la cantidad mínima de monedas necesarias
-            // para formar la cantidad cantActual, solo probando monedas menores a cantActual
-            for (int j = 0; j < monedas.length; j++) {
-                int monedaActual = monedas[j];
-                // número mínimo de monedas necesarias para formar la cantidad restante (cant - monedaActual).
-                int monedasNecesariasParaCantRestante = dp[cantActual - monedaActual];
-                if (monedaActual < cantActual && monedasNecesariasParaCantRestante != Integer.MAX_VALUE) {
-                    // número mínimo de monedas necesarias para formar la cantidad actual
-                    dp[cantActual] = Math.min(dp[cantActual], monedasNecesariasParaCantRestante + 1);
+    public static int cambio(int[] coins, int amount) {
+        int[] dp = new int[amount + 1];
+        Arrays.fill(dp, amount + 1);
+        dp[0] = 0;
+        for (int i = 1; i <= amount; i++) {
+            for (int j = 0; j < coins.length; j++) {
+                if (coins[j] <= i) {
+                    dp[i] = Math.min(dp[i], dp[i - coins[j]] + 1);
                 }
             }
         }
-        return dp[cantidad] == Integer.MAX_VALUE ? -1 : dp[cantidad];
+        return dp[amount] > amount ? -1 : dp[amount];
     }
 
     public static void main(String[] args) {
         int[] monedas = {1, 2, 5};
-        int cantidad = 11;
+        int cantidad = 7;
         System.out.println(cambio(monedas, cantidad));
     }
 }
